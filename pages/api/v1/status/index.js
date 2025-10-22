1cc64bdb6629c86f0399a94b3b80cd5fde55ad56
Maycon Douglas <40704841+mkydouglas@users.noreply.github.com>
2025-10-22 17:41:29 +0000

    finish `/api/v1/status` endpoit

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -9,11 +9,12 @@ async function status(request, response) {
   const max = await database.query("SHOW max_connections");
   const maxConnections = max.rows[0].max_connections;
 
-  const totalConnections = await database.query(
-    "SELECT COUNT(*)::int AS total_connections FROM pg_stat_activity WHERE datname = 'local_db' GROUP BY datname;",
-  );
+  const databaseName = process.env.POSTGRESS_DB;
+  const totalConnections = await database.query({
+    text: "SELECT COUNT(*)::int AS total_connections FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
   const tc = totalConnections.rows[0].total_connections;
-  console.log(tc);
 
   response.status(200).json({
     updated_at: updatedAt,
