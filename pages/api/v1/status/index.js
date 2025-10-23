77f97cdd3b69f78049def13f06a8706785fc1e49
Maycon Douglas <40704841+mkydouglas@users.noreply.github.com>
2025-10-23 12:50:21 +0000

    handles database ssl connection

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -9,13 +9,12 @@ async function status(request, response) {
   const max = await database.query("SHOW max_connections");
   const maxConnections = max.rows[0].max_connections;
 
-  const databaseName = process.env.POSTGRESS_DB;
+  const databaseName = process.env.POSTGRES_DB;
   const totalConnections = await database.query({
-    text: "SELECT COUNT(*)::int AS total_connections FROM pg_stat_activity WHERE datname = $1;",
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
     values: [databaseName],
   });
-  const tc = totalConnections.rows[0].total_connections;
-
+  const tc = totalConnections.rows[0].count;
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
